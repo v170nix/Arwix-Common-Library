c4aff1a5c71c2d75028675c28f2f11641da7b713
Vitaliy Sheyanov <vit.onix@gmail.com>
2019-11-23 21:35:39 +0300

    update

--- lib/src/main/java/net/arwix/extension/ContextExtensions.kt
+++ lib/src/main/java/net/arwix/extension/ContextExtensions.kt
@@ -9,3 +9,6 @@ import androidx.annotation.ColorInt
 fun Context.getThemeColor(@AttrRes attr: Int) = TypedValue().also {
     theme.resolveAttribute(attr, it, true)
 }.data
+
+fun Context.getString(identifierKey: String): String =
+    this.getString(resources.getIdentifier(identifierKey, "string", packageName))
--- lib/src/main/java/net/arwix/extension/ViewExtensions.kt
+++ lib/src/main/java/net/arwix/extension/ViewExtensions.kt
@@ -1,8 +1,13 @@
 package net.arwix.extension
 
 import android.content.Context
+import android.content.res.ColorStateList
+import android.os.Build
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.drawable.DrawableCompat
 
 fun <T : View> T.visible(): T {
     visibility = View.VISIBLE
@@ -19,9 +24,21 @@ fun <T : View> T.invisible(): T {
     return this
 }
 
+fun View.setBackgroundDrawableCompat(idDrawable: Int) {
+    this.background = DrawableCompat.wrap(ContextCompat.getDrawable(this.context, idDrawable)!!)
+}
+
 fun View.hideSoftInputFromWindow() {
     val imm = context.getSystemService(Context.INPUT_METHOD_SERVICE)?.let {
         it as InputMethodManager
     } ?: return
     imm.hideSoftInputFromWindow(this.windowToken, 0)
+}
+
+fun TextView.setTextAppearanceCompat(resId: Int) {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        this.setTextAppearance(resId)
+    } else {
+        this.setTextAppearance(this.context, resId)
+    }
 }
