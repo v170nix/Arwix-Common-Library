93f958403d85b565d0834275ba44c678b7e65aae
Vitaliy Sheyanov <vit.onix@gmail.com>
2020-06-15 15:05:54 +0300

    add notification channel

--- lib/src/main/java/net/arwix/extension/ContextExtensions.kt
+++ lib/src/main/java/net/arwix/extension/ContextExtensions.kt
@@ -1,6 +1,9 @@
 package net.arwix.extension
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Context
+import android.os.Build
 import android.util.TypedValue
 import androidx.annotation.AttrRes
 import androidx.annotation.ColorInt
@@ -18,4 +21,12 @@ fun Context.resolveAttribute(@AttrRes attributeResId: Int): Int {
     if (theme.resolveAttribute(attributeResId, typedValue, true))
         return typedValue.data
     throw IllegalArgumentException(resources.getResourceName(attributeResId))
+}
+
+fun Context.createNotificationChannel(id: String, createChannel: (id: String) -> NotificationChannel) {
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) return
+    val manager = getSystemService(Context.NOTIFICATION_SERVICE) as? NotificationManager ?: return
+    val channel = manager.getNotificationChannel(id)
+    if (channel != null) return
+    manager.createNotificationChannel(createChannel(id))
 }
