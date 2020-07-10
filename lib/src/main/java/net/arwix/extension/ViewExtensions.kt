32d16a37d375930498d9c81b2f11c799cd865750
Vitaliy Sheyanov <vit.onix@gmail.com>
2020-07-10 17:22:37 +0300

    add viewbinding

--- lib/src/main/java/net/arwix/extension/ViewExtensions.kt
+++ lib/src/main/java/net/arwix/extension/ViewExtensions.kt
@@ -1,15 +1,24 @@
 package net.arwix.extension
 
 import android.content.Context
-import android.content.res.ColorStateList
-import android.content.res.Resources
 import android.os.Build
 import android.util.TypedValue
+import android.view.LayoutInflater
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.graphics.drawable.DrawableCompat
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.observe
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewbinding.ViewBinding
+import kotlin.properties.ReadOnlyProperty
+import kotlin.reflect.KProperty
 
 fun <T : View> T.visible(): T {
     visibility = View.VISIBLE
@@ -54,3 +63,45 @@ fun View.toDp(dpValue: Float) =
 fun View.toSp(spValue: Int) = toSp(spValue.toFloat())
 
 fun View.toDp(dpValue: Int) = toDp(dpValue.toFloat())
+
+class FragmentViewBindingDelegate<T : ViewBinding>(
+    val fragment: Fragment,
+    val viewBindingFactory: (View) -> T
+) : ReadOnlyProperty<Fragment, T> {
+
+    private var binding: T? = null
+
+    init {
+        fragment.lifecycle.addObserver(object : DefaultLifecycleObserver {
+            override fun onCreate(owner: LifecycleOwner) {
+                fragment.viewLifecycleOwnerLiveData.observe(fragment) {
+                    it.lifecycle.addObserver(object : DefaultLifecycleObserver {
+                        override fun onDestroy(owner: LifecycleOwner) {
+                            binding = null
+                        }
+                    })
+                }
+            }
+        })
+    }
+
+    override fun getValue(thisRef: Fragment, property: KProperty<*>): T {
+        val binding = binding
+        if (binding != null) return binding
+        val lifecycle = fragment.viewLifecycleOwner.lifecycle
+        if (!lifecycle.currentState.isAtLeast(Lifecycle.State.INITIALIZED))
+            throw  IllegalStateException("bindings not allow when fragment views are destroyed")
+        return viewBindingFactory(thisRef.requireView()).also { this.binding = it }
+    }
+}
+
+fun <T : ViewBinding> Fragment.viewBinding(viewBindingFactory: (View) -> T) =
+    FragmentViewBindingDelegate(this, viewBindingFactory)
+
+inline fun <T : ViewBinding> AppCompatActivity.viewBinding(
+    crossinline bindingInflater: (LayoutInflater) -> T
+) = lazy(LazyThreadSafetyMode.NONE) { bindingInflater(layoutInflater) }
+
+inline fun <T: ViewBinding> RecyclerView.ViewHolder.viewBinding(
+    crossinline viewBindingFactory: (View) -> T
+) = lazy(LazyThreadSafetyMode.NONE) { viewBindingFactory(itemView) }
