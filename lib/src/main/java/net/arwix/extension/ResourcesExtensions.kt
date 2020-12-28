31a9e3fad2ffb438c837971d01fa7a2b8f8109c1
Vitaliy Sheyanov <vit.onix@gmail.com>
2020-12-28 11:59:00 +0300

    remove old extensions

--- lib/src/main/java/net/arwix/extension/ResourcesExtensions.kt
+++ lib/src/main/java/net/arwix/extension/ResourcesExtensions.kt
@@ -1,32 +1,7 @@
 package net.arwix.extension
 
-import android.content.Context
-import android.content.res.ColorStateList
 import android.content.res.Resources
-import android.graphics.drawable.Drawable
-import android.os.Build
 import android.util.TypedValue
-
-fun Resources.getCompatColor(id: Int, theme: Resources.Theme) =
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-        this.getColor(id, theme)
-    } else {
-        this.getColor(id)
-    }
-
-fun Resources.getCompatDrawable(id: Int, theme: Resources.Theme): Drawable =
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-        this.getDrawable(id, theme)
-    } else {
-        this.getDrawable(id)
-    }
-
-fun Resources.getColorStateListCompat(id: Int, theme: Resources.Theme): ColorStateList =
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-        this.getColorStateList(id, theme)
-    } else {
-        this.getColorStateList(id)
-    }
 
 fun Resources.toSp(spValue: Float) =
     TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_SP, spValue, displayMetrics)
