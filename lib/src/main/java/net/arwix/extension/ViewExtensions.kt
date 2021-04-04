a826daf267c493f7cf21418437ef7e6eb2e8acf5
Vitaliy Sheyanov <vit.onix@gmail.com>
2021-04-04 20:40:13 +0300

    small fix view binding

--- lib/src/main/java/net/arwix/extension/ViewExtensions.kt
+++ lib/src/main/java/net/arwix/extension/ViewExtensions.kt
@@ -2,6 +2,8 @@ package net.arwix.extension
 
 import android.content.Context
 import android.os.Build
+import android.os.Handler
+import android.os.Looper
 import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
@@ -14,7 +16,6 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.observe
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewbinding.ViewBinding
 import kotlin.properties.ReadOnlyProperty
@@ -70,14 +71,19 @@ class FragmentViewBindingDelegate<T : ViewBinding>(
 ) : ReadOnlyProperty<Fragment, T> {
 
     private var binding: T? = null
+    private val clearBindingHandler by lazy(LazyThreadSafetyMode.NONE) { Handler(Looper.getMainLooper()) }
 
     init {
+
         fragment.lifecycle.addObserver(object : DefaultLifecycleObserver {
             override fun onCreate(owner: LifecycleOwner) {
                 fragment.viewLifecycleOwnerLiveData.observe(fragment) {
                     it.lifecycle.addObserver(object : DefaultLifecycleObserver {
                         override fun onDestroy(owner: LifecycleOwner) {
-                            binding = null
+                            // Lifecycle listeners are called before onDestroyView in a Fragment.
+                            // However, we want views to be able to use bindings in onDestroyView
+                            // to do cleanup so we clear the reference one frame later.
+                            clearBindingHandler.post { binding = null }
                         }
                     })
                 }
@@ -86,6 +92,12 @@ class FragmentViewBindingDelegate<T : ViewBinding>(
     }
 
     override fun getValue(thisRef: Fragment, property: KProperty<*>): T {
+        // onCreateView may be called between onDestroyView and next Main thread cycle.
+        // In this case [binding] refers to the previous fragment view. Check that binding's root view matches current fragment view
+        if (binding != null && binding?.root !== thisRef.view) {
+            binding = null
+        }
+
         val binding = binding
         if (binding != null) return binding
         val lifecycle = fragment.viewLifecycleOwner.lifecycle
