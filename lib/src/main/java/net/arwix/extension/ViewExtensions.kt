3751fd65912a629c3ff841ab688453329aab948c
Vitaliy Sheyanov <vit.onix@gmail.com>
2021-04-04 20:50:15 +0300

    add view fade

--- lib/src/main/java/net/arwix/extension/ViewExtensions.kt
+++ lib/src/main/java/net/arwix/extension/ViewExtensions.kt
@@ -7,12 +7,16 @@ import android.os.Looper
 import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
 import androidx.core.graphics.drawable.DrawableCompat
+import androidx.core.view.children
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.interpolator.view.animation.FastOutSlowInInterpolator
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleOwner
@@ -117,3 +121,56 @@ inline fun <T : ViewBinding> AppCompatActivity.viewBinding(
 inline fun <T: ViewBinding> RecyclerView.ViewHolder.viewBinding(
     crossinline viewBindingFactory: (View) -> T
 ) = lazy(LazyThreadSafetyMode.NONE) { viewBindingFactory(itemView) }
+
+/**
+ * Fade a view to visible or gone. This function is idempotent - it can be called over and over again with the same
+ * value without affecting an in-progress animation.
+ */
+fun View.fadeTo(visible: Boolean, duration: Long = 500, startDelay: Long = 0, toAlpha: Float = 1f) {
+    // Make this idempotent.
+    val tagKey = "fadeTo".hashCode()
+    if (visible == isVisible && animation == null && getTag(tagKey) == null) return
+    if (getTag(tagKey) == visible) return
+
+    setTag(tagKey, visible)
+    setTag("fadeToAlpha".hashCode(), toAlpha)
+
+    if (visible && alpha == 1f) alpha = 0f
+    animate()
+        .alpha(if (visible) toAlpha else 0f)
+        .withStartAction {
+            if (visible) isVisible = true
+        }
+        .withEndAction {
+            setTag(tagKey, null)
+            if (isAttachedToWindow && !visible) isVisible = false
+        }
+        .setInterpolator(FastOutSlowInInterpolator())
+        .setDuration(duration)
+        .setStartDelay(startDelay)
+        .start()
+}
+
+/**
+ * Cancels the animation started by [fadeTo] and jumps to the end of it.
+ */
+fun View.cancelFade() {
+    val tagKey = "fadeTo".hashCode()
+    val visible = getTag(tagKey)?.castOrNull<Boolean>() ?: return
+    animate().cancel()
+    isVisible = visible
+    alpha = if (visible) getTag("fadeToAlpha".hashCode())?.castOrNull<Float>() ?: 1f else 0f
+    setTag(tagKey, null)
+}
+
+/**
+ * Cancels the fade for this view and any ancestors.
+ */
+fun View.cancelFadeRecursively() {
+    cancelFade()
+    castOrNull<ViewGroup>()?.children?.asSequence()?.forEach { it.cancelFade() }
+}
+
+private inline fun <reified T> Any.castOrNull(): T? {
+    return this as? T
+}
